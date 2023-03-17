f76460d2362349eb974fd1fb12651bcd158bf5b1
Larry_CNS <larrykuo.tech@gmail.com>
2023-03-17 18:05:14 +0800

    edit

--- galois_field.py
+++ galois_field.py
@@ -2,22 +2,31 @@ import galois
 import pprint
 import numpy as np
 print(galois.__version__)
-GF = galois.GF(3**2, repr="poly")
+GF = galois.GF(3**2, repr="int") # poly, int, power
 
 
 
 mult_table = [[GF(i)*GF(j) for j in range(3**2)] for i in range(3**2)]
 note_list = []
-for index,row in enumerate(mult_table):
-    for index2,column in enumerate(row) :
-        if "α + 1" == str(column) :
-            print( "(" + str(column) + ")", end="\t")
-            note_list.append((index,index2))
-        else :
-            print( column, end="\t")
-        
+for number in range(1,9) :
+    print(number)
+    for index,row in enumerate(mult_table):
+        for index2,column in enumerate(row) :
+            if index == 0 or index2 == 0 :
+                pass
+            elif str(number) == str(column) : # α + 1
+                print( "(" + str(column) + ")", end="\t")
+                note_list.append((index,index2))
+            elif index <= index2: # 2,2 3,3 3,4
+                print( "X", end="\t")
+            else:
+                print( end="\t")
+            
+        print()
+    print(note_list)
+    note_list.clear()
     print()
-print(note_list)
+
 
 f = GF.Random((3, 2), seed=1) ; f
 print(f)
