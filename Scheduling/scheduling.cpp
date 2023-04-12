95733e3da4014c9a25d4199160f7e7cd16915a86
Larry Kuo <larrykuo.tech@gmail.com>
2023-04-12 16:46:30 +0800

    change vector<int> to struct taskTime

--- Scheduling/scheduling.cpp
+++ Scheduling/scheduling.cpp
@@ -2,50 +2,79 @@
 # include <vector>
 # include <queue>
 # include <functional>
+# include <unordered_map>
 using namespace std ;
 
+struct taskTime {
+    int index ;
+    int excution ;
+    int period ;
+    pair<int,int> current ;
+    pair<int,int> deadline ;
+}; 
 
-auto deadline_compare = [](const vector<int>& p1, const vector<int>& p2) {
-    return p1[1] > p2[1];
+int addBigNumber( pair<int,int> time ) {
+    if ( time.first +1 == INT_MAX ) {
+        time.second ++ ;
+        time.first = 0 ;
+    }
+     
+} // bigNumber
+
+bool checkFinishCondition( vector<taskTime> & tasks) { // check next_period time is the same
+    for ( int i = 1 ; i < tasks.size() ; i ++ ) {
+        if (tasks[i-1].deadline.first != tasks[i].deadline.first || tasks[i-1].deadline.second != tasks[i].deadline.second )
+            return false ;
+    } // for
+
+    return true ;
+} // checkFinishCondition
+
+auto deadline_compare = [](const taskTime& p1, const taskTime& p2) {
+    return p1.deadline > p2.deadline;
 };
 
-auto rate_compare = [](const vector<int>& p1, const vector<int>& p2) {
-    return p1[1]/p1[2] > p2[1]/p2[2];
+auto rate_compare = [](const taskTime& p1, const taskTime& p2) {
+    return p1.excution/p1.period > p2.excution/p2.period;
 };
 
 class Compare {
 public:
-    bool operator()(const vector<int>& p1, const vector<int>& p2) {
+    bool operator()(const taskTime& p1, const taskTime& p2) {
         return comp(p1, p2);
     }
     
-    function<bool(const vector<int>&, const vector<int>&)> comp;
+    function<bool(const taskTime&, const taskTime&)> comp;
 };
 
 void Schedule( vector<vector<int> > task, int Display, int type ) {
     cout << "Scheduler Begin" << endl ;
     cout << task[0][1] << " " << task[1][1] << endl ;
-    vector<vector<int> > readylist ; // { index, excutionTime, period, current}
+    vector<taskTime> readylist ; // { index, excutionTime, period, current, deadline}
+    // vector<taskTime> readylist_map ;
     vector<int> currenttask, outputtask ;
-    priority_queue<vector<int>, vector<vector<int>>, Compare> pqueue;
+    priority_queue<taskTime, vector<taskTime>, Compare> pqueue;
+    // priority_queue<vector<taskTime>, vector<vector<taskTime>>, Compare> pqueue;
 
     if ( type == 0 ) {
-        pqueue = priority_queue<vector<int>, vector<vector<int>>, Compare>(Compare{deadline_compare});
+        pqueue = priority_queue<taskTime, vector<taskTime>, Compare>(Compare{deadline_compare});
     }
     else if ( type == 1 ) {
-        pqueue = priority_queue<vector<int>, vector<vector<int>>, Compare>(Compare{rate_compare});
+        pqueue = priority_queue<taskTime, vector<taskTime>, Compare>(Compare{rate_compare});
     }
     cout << "Success declare priority queue" << endl ;
     // priority queue
     bool start = true ;
     int preempted = 0 ;
-    int time = 0, taskaction_output = -1, taskaction_current  = 0 ; // task action: start, 0, end, 1, preempted, 2, resume, 3
+    int  taskaction_output = -1, taskaction_current  = 0 ; // task action: start, 0, end, 1, preempted, 2, resume, 3
+    pair<int,int> time ;
     for ( int i = 0 ; i < task.size() ; i ++ ) {
-        pqueue.push({i,task[i][0],task[i][1], task[i][0]}) ;
+        pqueue.push({i,task[i][0],task[i][1], task[i][0],task[i][1]}) ;
     } // for
     // push task to queue
     cout << "push task to queue" << endl ;
     // erase from readylist 
+    // checkFinishCondition() -> bool
     while( readylist.size()!= task.size() || ! pqueue.empty() ) { // 
         // cout << "Time: " << time << " Readylist length: " << readylist.size() << " pqueue: " << pqueue.size() << endl ;
         outputtask = currenttask ;
@@ -72,16 +101,13 @@ void Schedule( vector<vector<int> > task, int Display, int type ) {
             pqueue.push( newtask ) ;
         } // if
         else if ( newtask[3] == 0 ) {
-      
             int tempindex = newtask[0] ;
-            readylist.push_back( { tempindex, task[tempindex][0], task[tempindex][1], task[tempindex][0] }) ;
-    
-
+            readylist.push_back( { tempindex, task[tempindex][0], task[tempindex][1], task[tempindex][0], task[tempindex][4] }) ;
         }
         
         if ( !readylist.empty()) { // check period condition
             for ( int k = 0 ; k < readylist.size() ; k ++ ) {
-                if ( time % readylist[k][2] == 0 ) {
+                if ( timefront % readylist[k][2] == 0 ) {
                     pqueue.push( readylist[k] ) ;
                     readylist.erase(readylist.begin()+k ) ;
                     k -- ;
@@ -96,19 +122,22 @@ void Schedule( vector<vector<int> > task, int Display, int type ) {
 
         // output
         if ( Display == 1 ) {
-            cout << time << " " ;
+            cout << timefront << " " ;
             if ( !outputtask.empty()) {
                 cout << outputtask[1] << " " << taskaction_output << " " ;
             } // if
             cout << currenttask[1] << " " << taskaction_current << endl ;
         } // if
-        time ++ ;
+
+        addbigNumber(time) ;
     } // while
 
-    cout << " Total Time " << time+1 << " Preempted " << preempted << " Finished" << endl ;
+    cout << " Total Time " << timefront+1 << " Preempted " << preempted << " Finished" << endl ;
 } //
 
-
+// calculate deadline
+// big number
+// next period
 
 int main(int argc, char const *argv[])
 {
