86ebea8e284ed72388dcbba38d51690c563f7d5f
Larry_CNS <larrykuo.tech@gmail.com>
2023-04-30 15:43:32 +0800

    04302

--- link_prediction.py
+++ link_prediction.py
@@ -16,6 +16,12 @@ from sklearn.metrics import accuracy_score
 dataset = pd.read_csv("./new_train_data.csv")  
 
 
+
+# testset = dataset[:int(len(dataset)*1)]
+# print(len(dataset))
+# testset = dataset.sample(frac=0.3, random_state=42, replace=False)
+# print(len(testset))
+# print(len(dataset))
 G = nx.DiGraph()  
 G1 = nx.Graph()  
 
@@ -69,18 +75,14 @@ for edge in train_edges:
     # 獲取邊的兩個端點
     node1, node2 = edge # source -> target
     
-
     jacc, common_neighbors, pa1, adamic = calculate_score(node1,node2)
     indegree = G.in_degree(int(node1)) + G.in_degree(int(node2))+G.out_degree(int(node1)) + G.out_degree(int(node2))
     katz_score = katz[node2] + katz[node1]
     
-
     try :
-        shortest_path_length = -len(nx.shortest_path_length(G1, source=node1, target=node2))
+        shortest_path_length = -nx.shortest_path_length(G1, source=node1, target=node2)
     except :
         shortest_path_length = -100
-
-
     
     record.append([jacc, common_neighbors, pa1, adamic, shortest_path_length, indegree ])
  
@@ -96,13 +98,9 @@ for edge in test_edges:
     indegree = G.in_degree(int(node1)) + G.in_degree(int(node2))+G.out_degree(int(node1)) + G.out_degree(int(node2))
 
     try :
-        shortest_path_length = -len(nx.shortest_path_length(G1, source=node1, target=node2))
+        shortest_path_length = -nx.shortest_path_length(G1, source=node1, target=node2)
     except :
         shortest_path_length = -100
-
-    # test_Record[(node1,node2)].append(shortest_path_length)  
-    
-    # test_Record[(node1,node2)].append(label)
     
     test_Record.append([jacc, common_neighbors, pa1, adamic, shortest_path_length, indegree ])
 
@@ -110,28 +108,95 @@ for edge in test_edges:
 
 
         
-# testset = dataset[int(len(dataset)*0.3):]
+
 
 training_data = pd.DataFrame(record)
 
-print(training_data[[0]])
-  
-# print( training_data ) 
 training_data.to_csv('training_data.csv')
 
-# train_X = training_data[['Shortest', 'Common', 'Jaccard', 'PA1','adamic', 'katz']]
 
 
-######################
-classifier = LogisticRegression()
+
+sc_X = StandardScaler()
+# train_X = sc_X.fit_transform(train_X)
+# train_label = sc_X.transform(train_label)
+
+
+X_shortest = training_data[[0]]
+X_shortest = sc_X.fit_transform(X_shortest)
+
+X_common = training_data[[1]]
+X_common = sc_X.fit_transform(X_common)
+
+X_jaccard = training_data[[2]]
+X_jaccard = sc_X.fit_transform(X_jaccard)
+
+X_pa1 = training_data[[3]]
+X_pa1 = sc_X.fit_transform(X_pa1)
+
+X_adamic = training_data[[4]]
+X_adamic = sc_X.fit_transform(X_adamic)
+
+X_katz = training_data[[5]]
+X_katz = sc_X.fit_transform(X_katz)
+
+clf_shortest = SVC().fit(X_shortest, train_labels)
+clf_common = SVC().fit(X_common, train_labels)
+clf_jaccard = SVC().fit(X_jaccard, train_labels)
+clf_pa1 = SVC().fit(X_pa1, train_labels)
+clf_adamic = SVC().fit(X_adamic, train_labels)
+clf_katz = SVC().fit(X_katz, train_labels)
+
+classifier = VotingClassifier(estimators=[('shortest', clf_shortest), ('common', clf_common), ('jaccard', clf_jaccard),
+                                          ('pa1', clf_pa1), ('adamic', clf_adamic), ('katz', clf_katz)],
+                              voting='hard',
+                              weights=[1, 1, 1, 1, 1, 1]) #
+
 classifier.fit(record, train_labels)
 y_pred = classifier.predict(test_Record)
 print("Accuracy: %.2f%%" % (classifier.score(test_Record, test_labels) * 100.0))
+
+
+
+# # Fitting Logistic Regression to the Training set
+
+######################
+# classifier = SVC()
+# classifier.fit(record, train_labels)
+# y_pred = classifier.predict(test_Record)
+# print("Accuracy: %.2f%%" % (classifier.score(test_Record, test_labels) * 100.0))
 #######################
+
+
+
+
+
+# perform KFold cross validation
+# kf = KFold(n_splits=5, shuffle=True, random_state=42)
+# accuracy_scores = []
+
+# for train_indices, test_indices in kf.split(X):
+#     X_train, X_test = X.iloc[train_indices], X.iloc[test_indices]
+#     y_train, y_test = y.iloc[train_indices], y.iloc[test_indices]
+    
+#     # X_train, X_test = X[train_indices], X[test_indices]
+#     # y_train, y_test = y[train_indices], y[test_indices]
+    
+#     classifier.fit(X_train, y_train)
+#     y_pred = classifier.predict(X_test)
+    
+#     accuracy_scores.append(accuracy_score(y_test, y_pred))
+
+# # compute mean accuracy score
+# mean_accuracy_score = sum(accuracy_scores) / len(accuracy_scores)
+
+# print("Mean Accuracy Score: %.2f%%" % (mean_accuracy_score * 100.0))
+
 
 print("load predict csv")
 predict_set = pd.read_csv("./new_test_data.csv")
 predict_edge = predict_set[["node1","node2"]].values
+
 
 G.add_nodes_from(predict_set['node1'])
 G.add_nodes_from(predict_set['node2'])
@@ -144,21 +209,27 @@ for edge in predict_edge:
     # 獲取邊的兩個端點
     node1, node2 = edge # source -> target
     
-
     jacc, common_neighbors, pa1, adamic = calculate_score(node1,node2)
     katz_score = katz[node2] + katz[node1]
     indegree = G.in_degree(int(node1)) + G.in_degree(int(node2))+G.out_degree(int(node1)) + G.out_degree(int(node2))
 
     
     try :
-        shortest_path_length = -nx.len(shortest_path_length(G1, source=node1, target=node2))
+        shortest_path_length = -nx.shortest_path_length(G1, source=node1, target=node2)
     except :
         shortest_path_length = -100
 
-    # predict_dict[(node1,node2)].append(shortest_path_length)  
+
     
     predict_dict.append([jacc, common_neighbors, pa1, adamic, shortest_path_length, indegree ])
+        
 
+
+# predict_data = pd.DataFrame.from_dict(predict_dict, orient='index', columns=[
+#                                       'Shortest', 'Common', 'Jaccard', 'PA1', 'adamic', 'katz'])
+
+# predict_data.to_csv('predict_data.csv')
+# predict_X = predict_data[['Shortest', 'Common', 'Jaccard', 'PA1', 'adamic', 'katz']]
 predict_y = classifier.predict(predict_dict) 
 
 # print((predict_X))
@@ -166,5 +237,6 @@ predict_y
 predict_y.astype(int)
 predict_y = pd.DataFrame(predict_y, columns=["ans"] )["ans"].astype(int)
 predict_y.index = predict_y.index.rename("node_pair_id")
-
+# predict_y = predict_y.reset_index().rename(columns={"index":"node_pair_id"})
+# print(predict_y)
 predict_y.to_csv("./result.csv")
