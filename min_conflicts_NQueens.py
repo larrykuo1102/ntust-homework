cd10023d1c85ffb9c42b1f370c61d4f6dd7c75f9
Larry_CNS <larrykuo.tech@gmail.com>
2023-05-24 19:45:56 +0800

    0524

---
+++ min_conflicts_NQueens.py
@@ -0,0 +1,24 @@
+# min conflicts solver for NQueens problems
+from nqueens import *
+import time
+n = 8
+NQ = NQueens(n)
+timer = time.time()
+while(not NQ.allQueensSafe()):
+    minAttacks = n + 1 # n + 1 is greater than any possibility of attacks so this is guaranteed to get minimized
+    pickedQueen = NQ.pickRandomQueen() 
+
+    positions = NQ.availablePositions(pickedQueen)
+    minConflictPosition = (-1,-1)
+    for pos in positions: # iterate through all positions of pickedQueen and move to position of minimum conflict
+        NQ.moveQueen(pickedQueen,pos)
+        newNumberOfConflicts = NQ.specificQueenConflicts(pos)
+        if(newNumberOfConflicts < minAttacks ):
+            minConflictPosition = pos
+            minAttacks = newNumberOfConflicts
+        NQ.moveQueen(pos,pickedQueen) # move queen back
+
+    NQ.moveQueen(pickedQueen,minConflictPosition)# move queen to least conflict spot
+
+print(NQ.printBoard())
+print(time.time() - timer)
