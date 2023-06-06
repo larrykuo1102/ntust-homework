eee085878fd023218f10c0d12e3d590b3dab704f
Larry_CNS <larrykuo.tech@gmail.com>
2023-06-06 15:41:47 +0800

    0606

--- GF_python_file/galois_solve_N_queens.py
+++ GF_python_file/galois_solve_N_queens.py
@@ -1,15 +1,19 @@
 import galois
-
-n = 11
+import time
+import numba
+import numpy as np
+from math import trunc
+n = 10007 # 997 1009 10007
 queensum = n - 1
 solve = []
+mult_table = []
 
-GF = galois.GF(int(n)) 
-mult_table = [[GF(i)*GF(j) for j in range(int(n))] for i in range(int(n))]
+
+
 '''
 
-1. do galois field(n)
-
+1. do galois field(n) 
+O(n^2)
 2. get n in the galois field table
 
 3. do combination if it needed
@@ -27,7 +31,13 @@ else col = value
 '''
 
 
-
+@numba.jit(nopython=True)
+def get_mult_table(n):
+    mult_table = np.zeros((n, n), dtype=np.int64)
+    for i in range(n):
+        for j in range(n):
+            mult_table[i, j] = (i * j) % n
+    return mult_table
 
 def check_diagonal_queens(lst):
     n = len(lst)
@@ -51,26 +61,40 @@ def check_diagonal_queens(lst):
     return False
 
 
-def find_empty_positions(queens):
+
+
+def find_empty_positions(queens) :
     global queensum
-    occupied = [[False] * queensum for _ in range(queensum)]
-    row_pos = [False] * queensum
-    col_pos = [False] * queensum
-    # 设置已知皇后的位置
-    for queen in queens:
+    col_pos = [True] * queensum
+    row_pos = [True] * queensum
+    diag1 = [True] * (queensum*2-1)
+    diag2 = [True] * (queensum*2-1)
+
+    for queen in queens :
         row, col = queen
-        row_pos[row] = True
-        col_pos[col] = True
+        col_pos[col] = False
+        row_pos[row] = False
+        diag1[row + col] = False # 反斜
+        diag2[row - col + (queensum-1)] = False # 斜線
+    print("diag1", diag1)
+    print("diag2", diag2)
+    col_pos = [ index  for index,i in enumerate(col_pos) if i == True]
+    row_pos = [index for index,i in enumerate(row_pos) if i == True]
+    print(row_pos, col_pos)
+    empty_position = []
+    for row in row_pos :
+        for col in col_pos :
+            print()
+            if ( diag1[row + col ] == False  ) :
+                print("test", [row,col])
+            elif ( diag2[row - col + (queensum-1)] ==  False) :
+                print("test1", [row,col])
+            else :
+                empty_position.append([row,col])
+
+    return empty_position
 
 
-    # 找到空位置的行和列
-    empty_positions = []
-    for row in range(queensum):
-        for col in range(queensum):
-            if not row_pos[row] and not col_pos[col]:
-                empty_positions.append((row, col))
-
-    return empty_positions
 
 
 def get_Galois_position(n):
@@ -86,16 +110,36 @@ def get_Galois_position(n):
             
     return note_list
 
+def initial_Galois_table() :
+    global n 
+    global mult_table
+    if n > 2 **20 :
+        GF = galois.GF(int(n))
+        print(GF.ufunc_mode)
+        mult_table = [[GF(i)*GF(j) for j in range(n)] for i in range(n)]
+    else :
+        mult_table = get_mult_table(n)
+
 def run( n ) :
     global queensum
     print( queensum, "queens")
     solve = [-1 for i in range(queensum)]
-    print(solve)
-
+    # print(solve)
+    currtime = time.time()
+    initial_Galois_table()
+    print( time.time() - currtime)
+    
+    print(len(mult_table))
     for galois_index in range(1,n) :
         galois_pos = get_Galois_position(galois_index)
         print(galois_pos)
 
 
 
+print( galois.prev_prime(1000))
 run(n)
+# queens = [ [3, 7], [7, 6], [4, 5] ] # [2, 2], [6, 1], [1, 4], [5, 3]
+# queens = [[i[0]-1, i[1]-1] for i in queens]
+# print(queens)
+# ans = find_empty_positions(queens)
+# print(ans)
