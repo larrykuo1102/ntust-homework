a65f5404212abff8d444cc84950c22e65c2cd3de
Larry Kuo <larrykuo.tech@gmail.com>
2023-06-12 02:07:51 +0800

    0611

--- community_detection.py
+++ community_detection.py
@@ -1,28 +1,45 @@
-def process_data():
-    progress = 0
-    
-    # 進行一些初始化工作...
-    print("test")
-    while progress < 100:
-        # 執行處理步驟...
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import random
+import pandas as pd
+from networkx import community
+def makeSampleGraph():
+    '''
+    生成图
+    '''
+
+    dataset = pd.read_csv('./community_detection/train.csv') 
+    G = nx.Graph()
+    # dataset = dataset[:500]
+    # print(len(dataset))ㄌ
+    edge = dataset[["Node1","Node2"]].values
+    G.add_weighted_edges_from(list((x, y, 1) for x,y in edge))
+    # G.add_edges_from(edge)
+
+    return G
+
+
+sample_graph = makeSampleGraph()
+
+partition = nx.algorithms.community.louvain_communities(sample_graph)
+
+result_community = {}
+for index, i in enumerate( partition) :
+    for node in i :
+        result_community[node] = index
         
-        # 更新進度
-        progress += 10
-        
-        # 產生進度值
-        yield progress
-    
-    # 完成處理工作，返回最終結果
-    result = "處理結果"
-    yield (result, "1111")
+testdata = pd.read_csv('./community_detection/test.csv')
+testdata = testdata[['Node1','Node2']].values
+result = []
+for x, y in testdata :
+    # print(result_community.get(y,-2))
+    if (result_community.get(x,-1) == result_community.get(y, -2)) :
+        result.append(1)
+    else :
+        result.append(0)
+    # print(x, y)
 
-# 使用生成器函式
-generator = process_data()
-
-# 逐步獲取進度值
-for value in generator:
-    print(value)
-
-result = value
-print( result )
-# 獲取最終結果
+predict_y = pd.DataFrame(result, columns=["Category"] )["Category"].astype(int)
+predict_y.index = predict_y.index.rename("Id")
+predict_y.to_csv("./community_detection/sampleSubmission.csv")
